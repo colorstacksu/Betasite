b9f076ca723fca3e43ea07acb7a57890949dda9b
colorstacksu <colorstacksu@gmail.com>
2024-08-08 01:45:53 -0400

    made many changes to eboard page

--- frontend/src/Eboard.jsx
+++ frontend/src/Eboard.jsx
@@ -9,9 +9,9 @@ import denniseImage from './assets/dennise3.jpg';
 import denzelImage from './assets/denzel1.jpg';
 import ramonImage from './assets/ramon3.jpg';
 import lisbethImage from './assets/lisbeth.jpg';
-import constantImage from './assets/constant.jpg';
+import constantImage from './assets/constant1.jpg';
 import philipImage from './assets/philip.jpg';
-import victorImage from './assets/victor.jpg';
+import victorImage from './assets/victor1.jpg';
 import karringtonImage from './assets/karrington2.jpg';
 import eboardPic from './assets/atrium.jpg';
 import './App.css';
@@ -23,6 +23,7 @@ import './App.css';
 // );
 
 const ExecutiveBoard = () => {
+  const teams = ["Executive Board", "Internal Team", "External Team", "Academic Team"]
   const boardMembers = [
     {
       name: 'Helena Patino Cruz',
@@ -103,35 +104,44 @@ const ExecutiveBoard = () => {
       description: 'Computer Science, May 2026'
     },
   ];
-
+  const rows = [];
+  for (let i = 0; i < boardMembers.length; i += 3) {
+    rows.push(boardMembers.slice(i, i + 3));
+  }
   return (
-    <Container className="mt-5">
-        <div className="hero-section" 
+    <Container className="mt-5" >
+      <div className="hero-section" 
         style={{ backgroundImage: `url(${eboardPic})`, 
-            backgroundSize: '100%', // Adjusts the size of the background image
-            padding: '100px 0', 
-            textAlign: 'center', 
-            color: '#fff' 
+          backgroundSize: '100%', 
+          padding: '100px 0', 
+          textAlign: 'center', 
+          color: '#fff' 
         }}>
-            <h1>Meet the Student Leaders of ColorStack at KSU!</h1>
-        </div>
-      <h2>Executive Board</h2>
-      <Row>
-        {boardMembers.map((member, index) => (
-          <Col md={4} sm={6} key={index} className="mb-4">
-            <Card>
-              <Card.Img variant="top" src={member.imgSrc} alt={member.name} />
-              <Card.Body>
-                <Card.Title>{member.title}</Card.Title>
-                <Card.Text>
-                  {member.name}<br/>
-                  {member.description}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+          <h1>Meet the Student Leaders of ColorStack at KSU!</h1>
+      </div>
+      {rows.map((row, rowIndex, team) => (
+        <Row 
+          key={rowIndex} 
+          className="mb-4"
+          style={{borderRadius:'30px' ,backgroundColor: rowIndex % 2 === 0 ? 'white' : '#feff78' }}
+        >
+          <h4>{teams[rowIndex]}</h4>
+          {row.map((member, colIndex) => (
+            <Col md={4} sm={6} key={colIndex}>
+              <Card style={{marginTop:'2%', borderRadius:'30px',height:'97%',alignItems:'center'}}>
+                <Card.Img variant="top" src={member.imgSrc} alt={member.name} style={{marginTop:'5%',borderRadius:'30px',height: '75%',width: '85%'}}/>
+                <Card.Body>
+                  <Card.Title>{member.title}</Card.Title>
+                  <Card.Text>
+                    {member.name}<br/>
+                    {member.description}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ))}
     </Container>
   );
 };
