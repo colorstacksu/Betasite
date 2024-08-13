34e569d1c4752f49e2b672e910735aa4b984e8f3
colorstacksu <colorstacksu@gmail.com>
2024-08-13 00:17:04 -0400

    added pic

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,95 +1,73 @@
 import React from 'react';
-import { Container, Navbar, Nav, Button, Row, Col, Card, Image } from 'react-bootstrap';
+import './App.css';
+import { Container, Navbar, Nav, Button, Row, Col, Card, Image, Section } from 'react-bootstrap';
 import about from './assets/atrium.jpg';
 import gallery1 from "./assets/homepage_gallery/homepage_gallery1.jpg";
 import gallery2 from "./assets/homepage_gallery/homepage_gallery2.jpg";
 import gallery3 from "./assets/homepage_gallery/homepage_gallery3.jpg";
 import gallery4 from "./assets/homepage_gallery/homepage_gallery4.jpg";
 import gallery5 from "./assets/homepage_gallery/homepage_gallery5.jpg";
+import graduation from "./assets/graduation.png";
+import computer from "./assets/computer.png";
+import girl from "./assets/girl_symbol.png";
 import { FaLinkedin, FaInstagram, FaMapMarkedAlt, FaDiscord } from 'react-icons/fa';
 
 import logo from './assets/ColorStack.png';
 
 const About = () => {
   return (
-    <div className="hero-section" style={{ position: 'relative', textAlign: 'center', color: 'white' }}>
-      {/* <br></br>
-      <img
-        src={about}
-        alt="Background"
-        style={{ width: '100%', height: 'auto', filter: 'brightness(50%)' }}
-      />
-      <Container
-        fluid
-        style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
-      >
-        <Row></Row>
-        <Row>
-          <Col>
-            <h1>A team that invests in the futures of those left behind every day.</h1>
-            <p>We’re a tech nonprofit cultivating the strongest cross-campus community of its kind to deliver community building, academic support, and career development opportunities at scale.</p>
-            <div>
-              <Button variant="warning" className="mr-3">Join Us</Button>
-              <Button variant="light">Learn More</Button>
-            </div>
-          </Col>
-        </Row>
-      </Container> */}
-      <Container className="text-center my-5" style={{backgroundImage:`url(${gallery1})`,
-    backgroundSize: 'cover',  // Ensures the background image covers the entire container
-    backgroundPosition: 'center',  // Centers the image
-    width: '100%', 
-    height: '400px', 
-    padding: '100px' // Adjust the height as needed
-    }}>
-        <div style={{display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'}}><h1>Empowering Black and Latino Students to Launch Successful Technical Careers</h1>
-        </div>
-        <p>Join ColorStack at Kennesaw State University</p>
-        <Button variant="warning" className="mr-2">Join Us</Button>
-        <Button variant="outline-warning">Learn More</Button>
-      </Container>
+    <div className="hero-section">
+      <div className="homepage-image">
+        <Container className='main-img-txt'>
+          <h1>Empowering Black and Latino Students to Launch Successful Technical Careers</h1>          
+          <p>Join ColorStack at Kennesaw State University</p>
+          <Button variant="warning">Join Us</Button>
+          <Button variant="outline-warning" className='learn-more-btn'>Learn More</Button>
+        </Container>
+      </div>
       
       {/* Statistics */}
-      <Container className="my-5">
+      <div className="stats">
         <Row className="text-center">
           <Col md={4}>
-            <Card>
+            <Card className='card'>
               <Card.Body>
-                <Card.Title>90%</Card.Title>
-                <Card.Text>of our members identify as Black & Latinx</Card.Text>
+                <img src={graduation} className="stats-img" />
+                <Card.Title className='stats-num'>90%</Card.Title>
+                <Card.Title className='stats-txt'>of our members identify as Black & Latinx</Card.Title>
               </Card.Body>
             </Card>
           </Col>
           <Col md={4}>
-            <Card>
+            <Card className='card'>
               <Card.Body>
-                <Card.Title>89%</Card.Title>
-                <Card.Text>members study Computer Science, Information Systems, or Computer Engineering</Card.Text>
+                <img src={computer} className="stats-img" />
+                <Card.Title className='stats-num'>89%</Card.Title>
+                <Card.Title className='stats-txt'>members study Computer Science, Information Systems, or Computer Engineering</Card.Title>
               </Card.Body>
             </Card>
           </Col>
           <Col md={4}>
-            <Card>
+            <Card className='card'>
               <Card.Body>
-                <Card.Title>40%</Card.Title>
-                <Card.Text>of our members identify as women</Card.Text>
+                <img src={girl} className="stats-img" />
+                <Card.Title className='stats-num'>40%</Card.Title>
+                <Card.Text className='stats-txt'>of our members identify as women</Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
-      </Container>
+      </div>
 
       {/* Mission Statement */}
-      <Container className="my-5 text-center">
+      <div className="mission">
         <h2>Our Mission</h2>
         <p>ColorStack at Kennesaw State University is dedicated to increasing the number of Black and Latinx graduates who pursue and succeed in rewarding technical careers. We provide support, resources, and a community to help our members thrive.</p>
-      </Container>
+      </div>
 
       {/* Gallery */}
-      <Container className="my-5">
-        <h2 className="text-center">Our Gallery</h2>
+      <div className="gallery">
+        <h2>Our Gallery</h2>
         <Row>
           <Col md={2}><Image src= {gallery1} thumbnail /></Col>
           <Col md={2}><Image src= {gallery2} thumbnail /></Col>
@@ -97,7 +75,7 @@ const About = () => {
           <Col md={2}><Image src= {gallery4} thumbnail /></Col>
           <Col md={2}><Image src= {gallery5} thumbnail /></Col>
         </Row>
-      </Container>
+      </div>
 
       {/* Upcoming Events */}
       <Container className="my-5">
